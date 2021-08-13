5ab9589af2aface8d80dc87b1e70839ca4452f80
Siddharth Patel <spatel@igloosoftware.com>
2021-08-13 15:13:57 -0400

    Data Binding with Binding Adapters

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -67,7 +67,8 @@ class SleepTrackerFragment : Fragment() {
 
         sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
             it?.let {
-                adapter.data = it
+                //adapter.data = it
+                adapter.submitList(it)
             }
         })
 
